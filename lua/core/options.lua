249a06ebf311ff67bc282af240c14075c59c9659
listout <listout@protonmail.com>
2022-08-13 16:12:16 +0530

    nvim: options.lua: set foldenable to false
    Signed-off-by: listout <listout@protonmail.com>

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -49,6 +49,7 @@ opt.lazyredraw = true       -- Faster scrolling
 opt.synmaxcol = 240         -- Max column for syntax highlight
 opt.updatetime = 300        -- ms to wait for trigger an event
 opt.timeoutlen = 500        -- ms to wait for a mapped sequence to complete.
+opt.foldenable = false
 
 -----------------------------------------------------------
 -- Startup
