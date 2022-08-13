f07108dca88d47eeae67587011994c6179a7d035
listout <listout@protonmail.com>
2022-08-13 16:17:43 +0530

    nvim: colorscheme: moving to tokyonight colorscheme
    Signed-off-by: listout <listout@protonmail.com>

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,15 +1,7 @@
 -- Config in lua
-vim.g.nord_contrast = true
-vim.g.nord_borders = true
-vim.g.nord_disable_background = false
-vim.g.nord_italic = true
-vim.g.nord_uniform_diff_background = true
-
---[[
-   [vim.g.tokyonight_style = "night"
-   [vim.g.tokyonight_italic_functions = 1
-   ]]
+vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_italic_functions = 1
+vim.g.tokyonight_sidebars = {"packer", "qf", " vista_kind", "terminal"}
 
 -- Load the colorscheme
-require('nord').set()
--- vim.cmd[[colorscheme nord]]
+vim.cmd[[colorscheme tokyonight]]
