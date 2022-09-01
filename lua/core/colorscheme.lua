0f7f546630b59a8895016fc1c379140369e0f0c6
listout <listout@protonmail.com>
2022-09-01 16:10:32 +0530

    nvim: colorscheme: tokyonight color scheme
    Signed-off-by: listout <listout@protonmail.com>

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,3 +1,11 @@
 -- Config in lua
+vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+vim.g.tokyonight_italic_functions = true
+
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+
 -- Load the colorscheme
-vim.cmd[[colorscheme substrata]]
+vim.cmd[[colorscheme tokyonight]]
