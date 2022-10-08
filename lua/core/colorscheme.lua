d394a6ee6bae2474adbae1903072e3b7b9114dda
listout <listout@protonmail.com>
2022-10-08 12:31:04 +0530

    nvim: use catppuccin colorscheme for lua
    Signed-off-by: listout <listout@protonmail.com>

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,18 +1,4 @@
 -- Config in lua
-vim.g.tokyonight_style = "storm"
-vim.g.tokyonight_italic_functions = true
-vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-vim.g.tokyonight_italic_functions = true
-
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-
--- Load the colorscheme
-vim.cmd[[colorscheme tokyonight]]
---[[
-   [vim.g.moonlight_italic_comments = true
-   [vim.g.moonlight_italic_keywords = true
-   [vim.g.moonlight_italic_functions = true
-   [vim.g.moonlight_borders = true
-   [require('moonlight').set()
-   ]]
+vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
+require("catppuccin").setup()
+vim.api.nvim_command "colorscheme catppuccin"
