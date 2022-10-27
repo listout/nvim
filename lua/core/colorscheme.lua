de6bc2cef817539ab5270f523c9261efd035bf5f
listout <listout@protonmail.com>
2022-10-27 12:31:01 +0530

    nvim: colorscheme.lua: more verbose settings
    Signed-off-by: listout <listout@protonmail.com>

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,4 +1,41 @@
 -- Config in lua
-vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
-require("catppuccin").setup()
+require("catppuccin").setup({
+	flavour = "frappe", -- latte, frappe, macchiato, mocha
+	background = { -- :h background
+	light = "latte",
+	dark = "frappe",
+	},
+	compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+	transparent_background = false,
+	term_colors = true,
+	dim_inactive = {
+		enabled = false,
+		shade = "dark",
+		percentage = 0.15,
+	},
+	styles = {
+		comments = { "italic" },
+		conditionals = { "italic" },
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+		operators = {},
+	},
+	color_overrides = {},
+	custom_highlights = {},
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		telescope = true,
+		treesitter = true,
+		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+	},
+})
 vim.api.nvim_command "colorscheme catppuccin"
