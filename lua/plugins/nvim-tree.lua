8b0e19656ace0b0147f1f346fcec99e95d217064
listout <listout@protonmail.com>
2022-11-28 12:16:30 +0530

    nvim: nvim-tree: verbose config options

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,4 +3,21 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- OR setup with some options
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+	sort_by = "case_sensitive",
+	view = {
+		adaptive_size = true,
+		mappings = {
+			list = {
+				{ key = "u", action = "dir_up" },
+			},
+		},
+	},
+	renderer = {
+		group_empty = true,
+		highlight_opened_files = "all"
+	},
+	filters = {
+		dotfiles = true,
+	},
+})
