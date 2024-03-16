0ddb929851de1a21c61c67bb097a6a647b4a2e63
Brahmajit Das <brahmajit.xyz@gmail.com>
2024-03-17 03:25:11 +0530

    nvim: utils: for misc utils
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

---
+++ lua/utils.lua
@@ -0,0 +1,17 @@
+--
+-- Auto group utilites
+--
+
+local M = {}
+local cmd = vim.cmd
+
+function M.create_augroup(autocmds, name)
+    cmd('augroup ' .. name)
+    cmd('autocmd!')
+    for _, autocmd in ipairs(autocmds) do
+        cmd('autocmd ' .. table.concat(autocmd, ' '))
+    end
+    cmd('augroup END')
+end
+
+return M
