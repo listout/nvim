dd16f35ddd4e0c797ee642ee90342fc590b945e2
Brahmajit Das <brahmajit.xyz@gmail.com>
2024-03-17 03:30:35 +0530

    nvim: init: neovim init file
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

---
+++ init.lua
@@ -0,0 +1,21 @@
+-- If neovim remote is not installed, install it
+if vim.fn.executable('nvr') == 0 then
+	vim.api.nvim_command('!pip3 install --user --break-system-packages neovim-remote')
+end
+
+-- Load plugins
+require('keymaps')
+require('basics')
+require('ftsettings')
+require('appearance')
+require('plugins')
+require('plugins.lualine')
+require('plugins.autopair')
+require('plugins.colorscheme')
+require('plugins.lsp')
+require('plugins.completion')
+require('plugins.gitsigns')
+require('plugins.nvim-tree')
+require('plugins.treesitter')
+require('plugins.usnippet')
+require('plugins.vimtex')
