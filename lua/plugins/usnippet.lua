5d974edb13bbeb6e599ed6c5df3fda58068d3399
Brahmajit Das <brahmajit.xyz@gmail.com>
2024-03-17 03:29:07 +0530

    nvim: plugins: usnippet: for ulti-snippet settings
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

---
+++ lua/plugins/usnippet.lua
@@ -0,0 +1,5 @@
+vim.g.UltiSnipsExpandTrigger = "<c-tab>"
+vim.g.UltiSnipsJumpForwardTrigger = "<nop>"
+vim.g.UltiSnipsJumpBackwardTrigger = "<nop>"
+
+vim.g.UltiSnipsSnippetDirectories = { 'UltiSnips', 'my_snippets' }
