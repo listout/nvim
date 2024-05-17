5d501a82893307b432833a0a211b67d96f025c38
Brahmajit Das <brahmajit.xyz@gmail.com>
2024-05-18 03:30:27 +0530

    nvim: plugins: completion: minor fomatting
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -50,8 +50,8 @@ cmp.setup {
 		end,
 	},
 	window = {
-		 completion = cmp.config.window.bordered(),
-		 documentation = cmp.config.window.bordered(),
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
 		['<C-d>'] = cmp.mapping.scroll_docs(-4),
