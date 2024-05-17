0a2ca6ad7c197b24d39d6fb1f7b67871b5f88a43
Brahmajit Das <brahmajit.xyz@gmail.com>
2024-05-18 03:33:19 +0530

    nvim: plugins: nvim-tree: use updated config
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,32 +3,17 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- OR setup with some options
-require('nvim-tree').setup({
-	sort_by = "case_sensitive",
-	on_attach = "disable",
+require("nvim-tree").setup({
+	sort = {
+		sorter = "case_sensitive",
+	},
 	view = {
-		centralize_selection = false,
-		cursorline = true,
-		debounce_delay = 15,
-		width = 34,
-		side = "left",
-		preserve_window_proportions = false,
-		number = false,
-		relativenumber = false,
-		signcolumn = "yes",
+		width = 30,
 	},
 	renderer = {
 		group_empty = true,
-		highlight_opened_files = "all"
 	},
 	filters = {
 		dotfiles = true,
-	},
-	git = {
-		enable = true,
-		ignore = true,
-		show_on_dirs = true,
-		show_on_open_dirs = true,
-		timeout = 400,
 	},
 })
