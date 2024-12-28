1025c09d84680e523426f9f37f317e28b7db1480
Brahmajit Das <brahmajit.xyz@gmail.com>
2024-12-28 18:30:54 +0530

    nvim: new style init file
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

--- init.lua
+++ init.lua
@@ -3,15 +3,6 @@ if vim.fn.executable('nvr') == 0 then
 	vim.api.nvim_command('!pip3 install --user --break-system-packages neovim-remote')
 end
 
--- Options to add `gf` functionality inside `.lua` files.
--- https://github.com/sam4llis/nvim-lua-gf/blob/ca712497b2bab6351518917be219e9bfd8d63e4f/after/ftplugin/lua.lua
-_G.vim.opt_local.include = [[\v<((do|load)file|require)[^''"]*[''"]\zs[^''"]+]]
-_G.vim.opt_local.includeexpr = "substitute(v:fname,'\\.','/','g')"
-for _, path in pairs(_G.vim.api.nvim_list_runtime_paths()) do
-_G.vim.opt_local.path:append(path .. '/lua')
-end
-_G.vim.opt_local.suffixesadd:prepend('.lua')
-
 local function get_hostname()
 	local f = io.open("/etc/hostname")
 	local hostname = f:read("*a") or ""
@@ -20,21 +11,62 @@ local function get_hostname()
 	return hostname
 end
 
--- Load plugins
-require('keymaps')
-require('basics')
-require('ftsettings')
-require('appearance')
+require 'basics'
+require 'appearance'
+require 'keymaps'
+
+-- Load plugins on devices other than RPi
 if get_hostname() ~= "shoggoth" then
-	require('plugin')
-	require('plugins.lualine')
-	require('plugins.autopair')
-	require('plugins.colorscheme')
-	require('plugins.lsp')
-	require('plugins.completion')
-	require('plugins.gitsigns')
-	require('plugins.nvim-tree')
-	require('plugins.treesitter')
-	require('plugins.usnippet')
-	require('plugins.vimtex')
+	-- Bootstrap lazy.nvim
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not (vim.uv or vim.loop).fs_stat(lazypath) then
+		local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+		local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+		if vim.v.shell_error ~= 0 then
+			vim.api.nvim_echo({
+				{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+				{ out,                            "WarningMsg" },
+				{ "\nPress any key to exit..." },
+			}, true, {})
+			vim.fn.getchar()
+			os.exit(1)
+		end
+	end
+	vim.opt.rtp:prepend(lazypath)
+
+	---@type LazySpec
+	local plugins = 'plugins'
+
+	-- Configure plugins.
+	require('lazy').setup(plugins, {
+		ui = { border = 'rounded' },
+		dev = { path = vim.g.projects_dir },
+		install = {
+			-- Do not automatically install on startup.
+			missing = false,
+		},
+		-- Don't bother me when tweaking plugins.
+		change_detection = { notify = false },
+		-- None of my plugins use luarocks so disable this.
+		rocks = {
+			enabled = false,
+		},
+		performance = {
+			rtp = {
+				-- Stuff I don't use.
+				disabled_plugins = {
+					'gzip',
+					'netrwPlugin',
+					'rplugin',
+					'tarPlugin',
+					'tohtml',
+					'tutor',
+					'zipPlugin',
+				},
+			},
+		},
+	})
+
+	vim.cmd.colorscheme "carbonfox"
 end
+-- Load plugins on devices other than RPi
