07b91321dee1a120c8b890d9b6fd7fff804f8487
Brahmajit Das <brahmajit.xyz@gmail.com>
2024-12-29 14:05:36 +0530

    read header file (h) as c file
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

--- plugin/filetypes.lua
+++ plugin/filetypes.lua
@@ -6,16 +6,23 @@ local cmd = vim.cmd
 local u = require('utils')
 
 u.create_augroup({
-	{ 'BufRead,BufNewFile', '/tmp/nail-*', 'setlocal', 'ft=mail' },
-	{ 'BufRead,BufNewFile', '*s-nail-*', 'setlocal', 'ft=mail' },
-	{ 'BufRead,BufNewFile', '*mutt-*', 'setlocal', 'ft=mail' },
-	{ 'BufRead', '/tmp/*mutt-*', 'setlocal', 'tw=72' },
+	{ 'BufRead,BufNewFile', '/tmp/nail-*',  'setlocal', 'ft=mail' },
+	{ 'BufRead,BufNewFile', '*s-nail-*',    'setlocal', 'ft=mail' },
+	{ 'BufRead,BufNewFile', '*mutt-*',      'setlocal', 'ft=mail' },
+	{ 'BufRead',            '/tmp/*mutt-*', 'setlocal', 'tw=72' },
 }, 'ftmail')
 
 -- Autoremove unwanted whitespaces
 cmd [[
 	au BufRead,BufNewFile *mutt-* setfiletype mail
 ]]
+
+vim.filetype.add {
+	extension = {
+		h = "c",
+		scheme = "scheme",
+	},
+}
 
 -- Add in the following like to auto remove empty lines
 -- au BufWritePre * %s/\s\+$//e
