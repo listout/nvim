5b62a6fdcaf0604d1234a89d0629680ca77317a5
Brahmajit Das <brahmajit.xyz@gmail.com>
2024-12-29 13:53:46 +0530

    moving clipboard setting to separate file in plugin folder
    Signed-off-by: Brahmajit Das <brahmajit.xyz@gmail.com>

--- lua/basics.lua
+++ lua/basics.lua
@@ -10,7 +10,6 @@ local cache_dir = os.getenv('HOME') .. '/.cache/nvim/'
 -- General
 -----------------------------------------------------------
 opt.mouse = 'a'                       -- Enable mouse support
-opt.clipboard = 'unnamedplus'         -- Copy/paste to system clipboard
 opt.swapfile = false                  -- Don't use swapfile
 opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
 opt.history = 500                     -- Lines vim should remember
