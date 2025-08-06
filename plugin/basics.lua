726a448916993967893a019019f6af5bd2073d8e
Brahmajit Das <listout@listout.xyz>
2025-08-06 06:04:32 +0530

    plugin/basics.lua: always display sign column
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- plugin/basics.lua
+++ plugin/basics.lua
@@ -29,7 +29,7 @@ opt.incsearch = true
 opt.ignorecase = true
 opt.backspace = 'indent,eol,start'
 opt.exrc = true
-opt.signcolumn = 'auto' -- Display only when there is a sign to display
+opt.signcolumn = 'yes' -- Display only when there is a sign to display
 
 -----------------------------------------------------------
 -- Tabs, indent
