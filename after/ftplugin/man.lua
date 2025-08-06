db2610c742f2b4c721b7ff102cc6bf114eede557
Brahmajit Das <listout@listout.xyz>
2025-08-06 06:07:30 +0530

    after/ftplugin/man.lua: dont define signcolumn here
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- after/ftplugin/man.lua
+++ after/ftplugin/man.lua
@@ -1,6 +1,5 @@
 vim.opt_local.number = false
 vim.opt_local.relativenumber = false
-vim.opt_local.signcolumn = 'no'
 vim.opt_local.scrolloff = 999
 
 vim.keymap.set({ 'n', 'x' }, 'q', 'ZQ', { buffer = true, nowait = true })
