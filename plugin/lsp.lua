fb78aa1c7b820a581b4ef8661be09cf8ec67ace3
Brahmajit Das <listout@listout.xyz>
2025-08-06 06:02:32 +0530

    plugin/lsp.lua: dont set border here
    using winborder instead
    
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -64,22 +64,3 @@ vim.o.updatetime = 250
 -- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 vim.cmd [[autocmd! ColorScheme * highlight NormalFloat guibg=#4c4f69]]
 vim.cmd [[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
-
-local border = {
-	{ "╭", "FloatBorder" },
-	{ "─", "FloatBorder" },
-	{ "╮", "FloatBorder" },
-	{ "│", "FloatBorder" },
-	{ "╯", "FloatBorder" },
-	{ "─", "FloatBorder" },
-	{ "╰", "FloatBorder" },
-	{ "│", "FloatBorder" },
-}
-
--- To instead override globally
-local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-	opts = opts or {}
-	opts.border = opts.border or border
-	return orig_util_open_floating_preview(contents, syntax, opts, ...)
-end
