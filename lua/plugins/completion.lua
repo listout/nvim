9a7fae96d58b6caa1713c7b97e30abec25d3b22d
Brahmajit Das <listout@listout.xyz>
2025-09-04 12:12:25 +0530

    lua: plugins: completion: add keymap for aborting completion
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -93,6 +93,7 @@ return {
 					['<C-d>'] = cmp.mapping.scroll_docs(-4),
 					['<C-f>'] = cmp.mapping.scroll_docs(4),
 					['<C-Space>'] = cmp.mapping.complete(),
+					['<C-e>'] = cmp.mapping.abort(),
 					['<CR>'] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							if luasnip.expandable() then
