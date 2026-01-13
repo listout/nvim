4d934621751e5949f85b9b559c3330acafe165b2
Brahmajit Das <listout@listout.xyz>
2026-01-14 00:15:56 +0530

    init: When editing a file, always jump to the last cursor position
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- init.lua
+++ init.lua
@@ -6,5 +6,19 @@ end
 -- Change leader to a comma
 vim.g.mapleader = ','
 
+-- When editing a file, always jump to the last cursor position
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		if vim.g.leave_my_cursor_position_alone then
+			return
+		end
+
+		local last_line = vim.fn.line([['"]])
+		if last_line > 0 and last_line <= vim.fn.line("$") then
+			vim.cmd.normal({ args = { [[g'"]] }, bang = true })
+		end
+	end,
+})
+
 require("config.lazy")
 vim.cmd.colorscheme "jellybeans-nvim"
