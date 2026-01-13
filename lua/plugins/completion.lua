5d9d93ee0f58070ddafc999a984079d6ddd157b1
Brahmajit Das <listout@listout.xyz>
2026-01-13 21:03:51 +0530

    lua: plugins: completions: remove formatting for completion
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -29,23 +29,6 @@ return {
 			local lspkind = require('lspkind')
 
 			cmp.setup {
-				view = {
-					docs = {
-						auto_open = false
-					},
-				},
-				formatting = {
-					format = lspkind.cmp_format({
-						mode = "symbol_text",
-						menu = ({
-							buffer = "[Buffer]",
-							nvim_lsp = "[LSP]",
-							luasnip = "[LuaSnip]",
-							nvim_lua = "[Lua]",
-							latex_symbols = "[Latex]",
-						})
-					}),
-				},
 				snippet = {
 					expand = function(args)
 						require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
