93be902bd22d03b94ff45c6f4eedf9dc9772c055
Brahmajit Das <listout@listout.xyz>
2026-01-13 21:52:54 +0530

    lua: plugins: productivity: fzf customization for style and preview
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- lua/plugins/productivity.lua
+++ lua/plugins/productivity.lua
@@ -13,7 +13,18 @@ return {
 		"junegunn/fzf",
 		build = ":call fzf#install()"
 	},
-	{ "junegunn/fzf.vim" },
+	{
+		"junegunn/fzf.vim",
+		init = function()
+			vim.g.fzf_vim = {
+				grep_multi_line     = 1,
+				preview_window      = { 'hidden,right,50%,<70(up,40%)', 'ctrl-/' },
+				buffers_options     = '--style full --border-label " Open Buffers "',
+				files_options       = '--style full --border-label " Files "',
+				commits_log_options = '--graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr"',
+			}
+		end,
+	},
 	{ "tpope/vim-surround" },
 	{ "junegunn/vim-easy-align" },
 }
