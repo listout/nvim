b8f06039bde7cad5310362b8eb7debf216cbbb05
Brahmajit Das <listout@listout.xyz>
2026-02-06 06:21:54 +0530

    lua: plugins: completion: correct dependency for LuaSnip
    Make rafamadriz/friendly-snippets dependency for LuaSnip
    
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -10,9 +10,9 @@ return {
 		"L3MON4D3/LuaSnip",
 		version = "v2.*",
 		event = "InsertEnter",
-		build = "make install_jsregexp"
+		build = "make install_jsregexp",
+		dependencies = { "rafamadriz/friendly-snippets" },
 	},
-	{ "rafamadriz/friendly-snippets" },
 	{
 		"petertriho/cmp-git",
 		dependencies = "nvim-lua/plenary.nvim"
