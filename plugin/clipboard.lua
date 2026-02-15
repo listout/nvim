d5cb6142b62b2aedf3eea78397bb3ef56813aff5
Brahmajit Das <listout@listout.xyz>
2026-02-15 09:12:31 +0530

    plugin: clipboard: only use unnamedplus locally
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- plugin/clipboard.lua
+++ plugin/clipboard.lua
@@ -1,22 +1,17 @@
-local opt = vim.opt
-
-opt.clipboard = "unnamedplus"
-
 if vim.env.SSH_CONNECTION then
-	local function vim_paste()
-		local content = vim.fn.getreg '"'
-		return vim.split(content, "\n")
-	end
+	local osc52 = require("vim.ui.clipboard.osc52")
 
 	vim.g.clipboard = {
-		name = "OSC 52",
+		name = "osc52",
 		copy = {
-			["+"] = require("vim.ui.clipboard.osc52").copy "+",
-			["*"] = require("vim.ui.clipboard.osc52").copy "*",
+			["+"] = osc52.copy("+"),
+			["*"] = osc52.copy("*"),
 		},
 		paste = {
-			["+"] = vim_paste,
-			["*"] = vim_paste,
+			["+"] = osc52.paste("+"),
+			["*"] = osc52.paste("*"),
 		},
 	}
+else
+	vim.opt.clipboard = "unnamedplus"
 end
